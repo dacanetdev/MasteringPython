c322e4900ef3356f5f5930134917cfb3c9c1637e
David Carrillo <david.carrillo@unosquare.com>
2019-03-06 14:39:07 -0800

    Completed Web Site Blocker, started Flask Website project

--- websiteblocker/webblocker.py
+++ websiteblocker/webblocker.py
@@ -3,11 +3,11 @@ from datetime import datetime as dt
 
 host_path = "hosts"
 
-redirect = "localhost"
+redirect = "127.0.0.1"
 
 website_list = ["www.netflix.com", "www.twitter.com"]
 
-while true:
+while True:
   if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
     file = open(host_path,"r+")
     content = file.read()
@@ -15,7 +15,7 @@ while true:
       if website in content:
         pass
       else:
-        file.write(f"{redirect} {website}\n")
+        file.write(f"{redirect}\t{website}\n")
   else:
      file = open(host_path,"r+")
      content = file.readlines()
